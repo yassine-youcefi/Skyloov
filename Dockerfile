92611cf6f77383bca478f26b835a0d86e34d140c
yani <yanilacamora@gmail.com>
2023-05-05 17:15:28 +0100

    dockerize Skyloov project and setup mysql db

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+# syntax = docker/dockerfile:1.2.1
+
+FROM python:3
+
+ENV PYTHONUNBUFFERED 1
+ENV PYTHONDONTWRITEBYTECODE 1
+
+
+RUN apt-get update && apt-get install -y tzdata
+RUN apt install libpq-dev nginx -y
+# RUN pip install gunicorn
+
+# Set work directory
+WORKDIR /code 
+COPY requirements.txt /code/
+RUN pip install --upgrade pip
+RUN --mount=type=cache,target=/root/.cache \    
+    pip install -r requirements.txt 
+
+
+EXPOSE 8080
+COPY . /code/
+# CMD ["gunicorn", "--bind", ":8080", "--workers", "3", "config.wsgi"]
--- config/settings.py
+++ config/settings.py
@@ -61,12 +61,23 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+'default': {
+    'ENGINE': 'django.db.backends.mysql',
+    'NAME': 'tg_db',
+    'USER': 'root',
+    'PASSWORD': 'password',
+    'HOST': 'db',
+    'PORT': '3306',
+    'OPTIONS': {'init_command': 'SET default_storage_engine=INNODB;'}
+},} 
 
 
 # Password validation
